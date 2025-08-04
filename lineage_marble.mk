5cdaf1a64418a3d3b6682047556b37e68025209d
beingashwani <ashwanic177@gmail.com>
2025-08-04 13:09:03 +0530

    marble: AxionOS initial bringup

--- lineage_marble.mk
+++ lineage_marble.mk
@@ -23,3 +23,26 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     SystemName=marble_global
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+#AxionAosp Flags
+
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_DISABLE_EPPE := true
+TARGET_ENABLE_BLUR := true
+BYPASS_CHARGE_SUPPORTED := true
+
+# Maintainer name (use "_" for spaces, e.g., "rmp_22" → "rmp 22" in UI)
+AXION_MAINTAINER := Ashwani
+
+# Processor name (use "_" for spaces)
+AXION_PROCESSOR := Snapdragon®_7+_Gen_2_(SM7475)
+
+# Define rear camera specs (multiple sensors supported)
+AXION_CAMERA_REAR_INFO := 64,8,2  # Example: 50MP + 48MP
+
+# Define front camera specs
+AXION_CAMERA_FRONT_INFO := 16  # Example: 42MP
+
+#cpu
+AXION_CPU_SMALL_CORES := 0,1,2,3
+AXION_CPU_BIG_CORES := 4,5,6,7
